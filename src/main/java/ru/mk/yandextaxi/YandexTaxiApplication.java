f6baa6ee69a7ff3da255c92c712ef26a60e3add2
droodcore <ensoxox@gmail.com>
2023-05-06 22:48:23 +0500

    first commit w/ basic config added

---
+++ src/main/java/ru/mk/yandextaxi/YandexTaxiApplication.java
@@ -0,0 +1,13 @@
+package ru.mk.yandextaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexTaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexTaxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mk/yandextaxi/YandexTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mk.yandextaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexTaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
